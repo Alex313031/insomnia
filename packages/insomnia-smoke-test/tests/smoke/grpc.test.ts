ef8c6c0b7c227516a5e10c261da3a008ca40f680
George He <hexxa@outlook.com>
2023-11-29 17:31:15 +0800

    fix: add tests for client side streaming and bidirectional streaming

--- packages/insomnia-smoke-test/tests/smoke/grpc.test.ts
+++ packages/insomnia-smoke-test/tests/smoke/grpc.test.ts
@@ -4,13 +4,11 @@ import { loadFixture } from '../../playwright/paths';
 import { test } from '../../playwright/test';
 
 test.describe('test grpc requests', async () => {
-  test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
-  let statusTag: any, responseBody: any;
+  test('test unary request', async ({ app, page }) => {
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
 
-  // import the proto
-  test.beforeEach(async ({ app, page }) => {
-    statusTag = page.locator('[data-testid="response-status-tag"]:visible');
-    responseBody = page.locator('[data-testid="response-pane"] >> [data-testid="CodeEditor"]:visible', {
+    const statusTag = page.locator('[data-testid="response-status-tag"]:visible');
+    const responseBody = page.locator('[data-testid="response-pane"] >> [data-testid="CodeEditor"]:visible', {
       has: page.locator('.CodeMirror-activeline'),
     });
 
@@ -24,88 +22,93 @@ test.describe('test grpc requests', async () => {
     await page.getByRole('button', { name: 'Scan' }).click();
     await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
     await page.getByText('CollectionPreRelease gRPCjust now').click();
+
+    // choose request
+    await page.getByLabel('Request Collection').getByTestId('UnaryWithOutProtoFile').press('Enter');
+    await expect(page.getByRole('button', { name: 'Select Method' })).toBeDisabled();
+    await page.getByTestId('button-server-reflection').click();
+
+    // choose method
+    await page.getByRole('button', { name: 'Select Method' }).click();
+    await page.getByRole('menuitem', { name: 'RouteGuide/GetFeature' }).click();
+
+    // start
+    await page.getByRole('button', { name: 'Send' }).click();
+
+    // verify
+    await page.getByRole('tab', { name: 'Response 1' }).click();
+    await expect(statusTag).toContainText('0 OK');
+    await expect(responseBody).toContainText('Berkshire Valley Management Area Trail');
   });
 
-  const testCases = [
-    {
-      name: 'test unary request',
-      methodName: 'RouteGuide/GetFeature',
-      clickStart: false,
-      clickCommit: false,
-      clickTab: true,
-      expectedStatus: '0 OK',
-      expectedBody: 'Berkshire Valley Management Area Trail',
-    },
+  test('test client side stream', async ({ app, page }) => {
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
 
-    // shoud update body as
-    // {
-    //   "lo": {
-    //     "latitude":"409146138",
-    //     "longitude":"-746188906"
-    //   },
-    //   "hi": {
-    //     "latitude":"409146138",
-    //     "longitude":"-746188906"
-    //   }
-    // }
-
-    // {
-    //   name: 'test server side stream',
-    //   methodName: 'RouteGuide/ListFeatures',
-    //   clickCommit: false,
-    //   clickTab: true,
-    //   expectedStatus: '0 OK',
-    //   expectedBody: 'Berkshire Valley Management Area Trail, Jefferson, NJ, USA',
-    // },
-    {
-      name: 'test client side stream',
-      methodName: 'RouteGuide/RecordRoute',
-      clickStart: true,
-      clickCommit: true,
-      clickTab: true,
-      expectedStatus: '0 OK',
-      expectedBody: 'point_count": 0',
-    },
-    {
-      name: 'test bidirectional stream',
-      methodName: 'RouteGuide/RouteChat',
-      clickStart: true,
-      clickCommit: true,
-      clickTab: false,
-      expectedStatus: '0 OK',
-      expectedBody: '', // TODO: should verify response
-    },
-  ];
-
-  for (const tc of testCases) {
-    test(tc.name, async ({ page }) => {
-      // choose request
-      await page.getByLabel('Request Collection').getByTestId('UnaryWithOutProtoFile').press('Enter');
-      await expect(page.getByRole('button', { name: 'Select Method' })).toBeDisabled();
-      await page.getByTestId('button-server-reflection').click();
-
-      // choose method
-      await page.getByRole('button', { name: 'Select Method' }).click();
-      await page.getByRole('menuitem', { name: tc.methodName }).click();
-
-      // start
-      if (tc.clickStart) {
-        await page.getByRole('button', { name: 'Start' }).click();
-      } else {
-        await page.getByRole('button', { name: 'Send' }).click();
-      }
-      if (tc.clickCommit) {
-        await page.getByRole('button', { name: 'Commit' }).click();
-      }
-
-      // verify
-      if (tc.clickTab) {
-        await page.getByRole('tab', { name: 'Response 1' }).click();
-      }
-      await expect(statusTag).toContainText(tc.expectedStatus);
-      if (tc.expectedBody) {
-        await expect(responseBody).toContainText(tc.expectedBody);
-      }
+    const statusTag = page.locator('[data-testid="response-status-tag"]:visible');
+    const responseBody = page.locator('[data-testid="response-pane"] >> [data-testid="CodeEditor"]:visible', {
+      has: page.locator('.CodeMirror-activeline'),
     });
-  }
+
+    await page.getByRole('button', { name: 'Create in project' }).click();
+
+    const text = await loadFixture('grpc.yaml');
+    await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+    await page.getByRole('menuitemradio', { name: 'Import' }).click();
+    await page.locator('[data-test-id="import-from-clipboard"]').click();
+    await page.getByRole('button', { name: 'Scan' }).click();
+    await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+    await page.getByText('CollectionPreRelease gRPCjust now').click();
+
+    // choose request
+    await page.getByLabel('Request Collection').getByTestId('UnaryWithOutProtoFile').press('Enter');
+    await expect(page.getByRole('button', { name: 'Select Method' })).toBeDisabled();
+    await page.getByTestId('button-server-reflection').click();
+
+    // choose method
+    await page.getByRole('button', { name: 'Select Method' }).click();
+    await page.getByRole('menuitem', { name: 'RouteGuide/RecordRoute' }).click();
+
+    // start
+    await page.getByRole('button', { name: 'Send' }).click();
+    await page.getByRole('button', { name: 'Commit' }).click();
+
+    // verify
+    await page.getByRole('tab', { name: 'Response 1' }).click();
+    await expect(statusTag).toContainText('0 OK');
+    await expect(responseBody).toContainText('point_count": 0');
+  });
+
+  test('test bidirectional stream', async ({ app, page }) => {
+    test.slow(process.platform === 'darwin' || process.platform === 'win32', 'Slow app start on these platforms');
+
+    const statusTag = page.locator('[data-testid="response-status-tag"]:visible');
+
+    await page.getByRole('button', { name: 'Create in project' }).click();
+
+    const text = await loadFixture('grpc.yaml');
+    await app.evaluate(async ({ clipboard }, text) => clipboard.writeText(text), text);
+
+    await page.getByRole('menuitemradio', { name: 'Import' }).click();
+    await page.locator('[data-test-id="import-from-clipboard"]').click();
+    await page.getByRole('button', { name: 'Scan' }).click();
+    await page.getByRole('dialog').getByRole('button', { name: 'Import' }).click();
+    await page.getByText('CollectionPreRelease gRPCjust now').click();
+
+    // choose request
+    await page.getByLabel('Request Collection').getByTestId('UnaryWithOutProtoFile').press('Enter');
+    await expect(page.getByRole('button', { name: 'Select Method' })).toBeDisabled();
+    await page.getByTestId('button-server-reflection').click();
+
+    // choose method
+    await page.getByRole('button', { name: 'Select Method' }).click();
+    await page.getByRole('menuitem', { name: 'RouteGuide/RouteChat' }).click();
+
+    // start
+    await page.getByRole('button', { name: 'Start' }).click();
+    await page.getByRole('button', { name: 'Commit' }).click();
+
+    // verify
+    await expect(statusTag).toContainText('0 OK');
+  });
 });
